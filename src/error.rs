828c06e872b0d717e181136f4a344b9ccc77947f
Callum Leslie <git@cleslie.uk>
2025-09-02 21:56:53 +0100

    fix: implement error for error types
    Fixes the failing doctests

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,21 @@
+use thiserror::Error;
+
 /// Errors that can occur when using the suck channel
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Error, Debug, Clone, PartialEq, Eq)]
 pub enum Error {
     /// Channel has been closed gracefully
+    #[error("Channel closed")]
     ChannelClosed,
 
     /// Producer has disconnected unexpectedly
+    #[error("Producer disconnected")]
     ProducerDisconnected,
 
     /// No value source has been set
+    #[error("Producer has not set a source value")]
     NoSource,
 
     /// Internal error (e.g., mutex poisoning or source execution failure)
+    #[error("Internal error occured")] // TODO: Expand on this
     InternalError,
 }
