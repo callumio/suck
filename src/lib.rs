72b87fd6e0a90fe4cd583ab0124c00a885dc608a
Callum Leslie <git@cleslie.uk>
2026-03-04 20:56:09 +0000

    chore: reorganize module exports for async and sync features

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,21 @@
 
 #[cfg(feature = "async")]
 pub mod async_channel;
+#[cfg(feature = "sync")]
 pub mod channel;
 pub mod error;
+#[cfg(feature = "sync")]
 pub mod traits;
 
 #[cfg(feature = "async")]
 pub mod asynchronous;
 #[cfg(feature = "sync")]
 pub mod sync;
+#[cfg(any(feature = "sync", feature = "async"))]
 pub mod types;
 
 #[cfg(feature = "async")]
 pub use async_channel::{AsyncSourcer, AsyncSucker};
+#[cfg(feature = "sync")]
 pub use channel::{Sourcer, Sucker};
 pub use error::Error;
